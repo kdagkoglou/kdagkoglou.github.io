0e5718a41ff6bc83310076ad3548f09fad297006
kdagkoglou <33607289+kdagkoglou@users.noreply.github.com>
2017-11-14 11:38:26 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the Form field of the email.
-$from = 'info@kostasdagkoglou.com';
+$from = 'info@kdagkoglou.github.io';
 
 // an email address that will receive the email with the output of the form.
 $sendTo = 'k.dagkoglou@gmail.com';
@@ -14,7 +14,7 @@ $subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
